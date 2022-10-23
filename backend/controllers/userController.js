bff095901d6f608f157f16898f8b19e90d754730
SteveW1004 <lmnwsy@gmail.com>
2022-10-23 20:27:44 +1300

    add register user in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,3 +42,33 @@ export const getUserProfile = asyncHandler(async (req, res) => {
         throw new Error('User not found')
     }
 })
+
+// @desc Register a new user
+// @route GET /api/users
+// @access Public
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // password encryption in the hook of password changed in userModel.js
+    const user = await User.create({ name, email, password })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
 import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
 router.post('/login', authUser)
 router.route('/profile').get(verifyToken, getUserProfile)
+router.route('/').post(registerUser)
 
 export default router
