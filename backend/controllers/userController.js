2d5ca58dfa888a696048fa0e71c6de8871da1d95
SteveW1004 <lmnwsy@gmail.com>
2022-10-23 19:59:34 +1300

    add get user profile controller and add verify token middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
-import generateToken from '../utils/generateToken.js';
+import generateToken from '../utils/generateToken.js'
 
 // @desc Auth user and get token
 // @route GET /api/users/login
@@ -20,6 +20,25 @@ export const authUser = asyncHandler(async (req, res) => {
         })
     } else {
         res.status(401)
-        throw new Error('Invalid email or password');
+        throw new Error('Invalid email or password')
+    }
+})
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
     }
 })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { authUser } from '../controllers/userController.js'
+import { authUser, getUserProfile } from '../controllers/userController.js'
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(verifyToken, getUserProfile)
 
 export default router
