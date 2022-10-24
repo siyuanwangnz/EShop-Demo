ec901015feae79b4d1a0ef24758b7fee6cb914a9
SteveW1004 <lmnwsy@gmail.com>
2022-10-24 15:05:53 +1300

    update header component and set login router

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,28 +1,45 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogout } from '../../redux/slices/userSlice'
 
 export default function Header() {
+
+    // get action and reducer from redux store
+    const dispatch = useDispatch()
+    const { userInfo, status, error } = useSelector((state) => state.user)
+
+    const logoutHandler = () => {
+        dispatch(userLogout())
+    }
+
     return (
         <header>
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
                     <Link className="navbar-brand" style={{ fontSize: "1.6rem", fontWeight: "bold" }} to="/">DEMO</Link>
                     <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/cart">
-                                <i className="fas fa-shopping-cart" />
-                                CART
-                            </Link>
-                        </li>
-                        <li className="nav-item">
+                        <Link className="nav-link" to="/cart">
+                            <i className="fas fa-shopping-cart" />{' '}CART
+                        </Link>
+                        {userInfo ? (
+                            <li className="nav-item dropdown" id='username'>
+                                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fa-solid fa-user"></i>{' '}{userInfo.name}
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end">
+                                    <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
+                                    <li><button className="dropdown-item" onClick={logoutHandler}>Logout</button></li>
+                                </ul>
+                            </li>
+                        ) : (
                             <Link className="nav-link" to="/login">
-                                <i className="fas fa-user" />
-                                LOG IN
+                                <i className="fas fa-user" />{' '}LOG IN
                             </Link>
-                        </li>
+                        )}
                     </ul>
                 </div>
-            </nav>
+            </nav >
         </header >
     )
 }
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import Home from '../pages/Home'
 import Product from '../pages/Product'
 import Cart from '../pages/Cart'
+import Login from '../pages/Login'
 import { Navigate } from 'react-router-dom'
 
 export default [
@@ -9,6 +10,9 @@ export default [
     },
     {
         path: "/products/:id", element: <Product />
+    },
+    {
+        path: "/login", element: <Login />
     },
     {
         path: "/cart/:id", element: <Cart />
