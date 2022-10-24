ce0554f71d3b0cfcf5d04a91d5b6e2c215ae4526
SteveW1004 <lmnwsy@gmail.com>
2022-10-24 18:43:04 +1300

    add update user  profile in user controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router()
 
 router.post('/login', authUser)
-router.route('/profile').get(verifyToken, getUserProfile)
+router
+    .route('/profile')
+    .get(verifyToken, getUserProfile)
+    .put(verifyToken, updateUserProfile)
 router.route('/').post(registerUser)
 
 export default router
