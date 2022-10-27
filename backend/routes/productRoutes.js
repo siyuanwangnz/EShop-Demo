cfb9e2ec576cb9f0e37a3b535bff7299a9e85b18
Steve.Wang <lmnwsy@gmail.com>
2022-10-27 15:44:43 +1300

    separate frontend
    change proxy to lambda function

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { fetchProducts, fetchProductById } from '../controllers/productController.js'
-
-const router = express.Router()
-
-router.route('/').get(fetchProducts)
-router.route('/:id').get(fetchProductById)
-
-export default router
